43445ac7440df12a36ec2413b8069742fa07ad3b
fabio loterio <vitorloterio@gmail.com>
2020-10-14 22:01:20 -0300

    min 31:42

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from 'Header'
 
 function App() {
+  
+
   return (
-    <div className="App">
-      <h1>Minhas séries</h1><br/>
+    <div>
+      <Header />
     </div>
   )
 }
